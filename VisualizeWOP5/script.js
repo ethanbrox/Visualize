4d4e54a02ce53317cf005503021eff35a63d8e46
Ethan Brox <brox.ethan@gmail.com>
2017-10-02 17:05:08 -0500

    I am starting to redo the layout for the way this works

--- VisualizeWOP5/script.js
+++ VisualizeWOP5/script.js
@@ -1,4 +1,4 @@
-var analyser, canvas, ctx, random = Math.random;
+var analyser, canvas, ctx, random = Math.random, c, going;
 
 window.onload = function(){
     canvas = document.createElement('canvas');
@@ -8,11 +8,13 @@ window.onload = function(){
     ctx = canvas.getContext('2d');
     setupWebAudio();
     draw();
+    c = 0;
+    going = true;
 }
 
 function setupWebAudio(){
     var audio = document.createElement('audio');
-    audio.src = '01 Forever.mp3';
+    audio.src = 'Sounds/01 Sweet Lovin.mp3';
     audio.controls = 'true';
     document.body.appendChild(audio);
     audio.style.width = window.innerWidth + 'px';
@@ -30,12 +32,15 @@ function draw(){
     var freqByteData = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(freqByteData);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.fillStyle = 'black';
+    //ctx.fillStyle = 'rgb(20, 20, 20)';
+    
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     var dif = canvas.width / (freqByteData.length / 5);
     
     for(var i = 1; i < freqByteData.length; i += 10){
         ctx.fillStyle = "rgb(" + freqByteData[i] + "," + freqByteData[i] + "," + freqByteData[i] + ")";
+        //ctx.fillStyle = "rgb(" + freqByteData[i] + ", 0, 0 )";
+        //ctx.fillStyle = "rgb(0, 0, " + freqByteData[i] + ")";
         ctx.fillRect(i * 1.4, canvas.height / 2 - freqByteData[i], dif, canvas.height / 2);
     }
 }
